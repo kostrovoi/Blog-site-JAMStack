1d9c3fcd07399f74ae870bb03cfdf8771e62ec0e
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2021-07-04 13:24:56 +0300

    11tyfied-static-pages

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+// 11ty.dev/docs/
+
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  // eleventyConfig.addPassthroughCopy('./src/assets/blog');
+  eleventyConfig.addPassthroughCopy('./src/admin');
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public'
+    },
+  };
+};
